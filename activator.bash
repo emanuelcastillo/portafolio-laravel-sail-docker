4f14b74ea274fc3f0fefff61d2043b97d8631525
emanuelcastillo <emanuel.castillo0118@gmail.com>
2023-10-22 20:12:44 -0600

    Update activator.bash

--- activator.bash
+++ activator.bash
@@ -5,58 +5,52 @@ RED='\033[0;31m'
 NOCOLOR='\033[0;32m'
 YELL='\033[1;35m'
 echo -e "${RED}Comando creado por ${NOCOLOR}https://github.com/emanuelcastillo"
+
+# Comprobar si Composer está instalado
 if ! command -v composer &>/dev/null; then
     echo "Composer no está instalado. Por favor, instala Composer y vuelve a ejecutar el script."
     exit 1
 fi
-composer install
-# Ensure that Docker is running...
-if [ $? -ne 0 ]; then
-    echo "Docker is not running."
-    
-    exit 1
-fi
-
-# Define el alias sail
-SAIL_ALIAS='alias sail="[ -f sail ] && sh sail || sh vendor/bin/sail"'
-
-# Verifica si el archivo ~/.bashrc existe y si no, créalo
-if [ ! -f "$HOME/.bashrc" ]; then
-    touch "$HOME/.bashrc"
-fi
-
-# Verifica si el alias ya está en ~/.bashrc
-if ! grep -qF "$SAIL_ALIAS" "$HOME/.bashrc"; then
-    # Agrega el alias al archivo ~/.bashrc
-    echo "$SAIL_ALIAS" >> "$HOME/.bashrc"
-fi
-
-# Verifica si el archivo ~/.bash_profile existe y si no, créalo
-if [ ! -f "$HOME/.bash_profile" ]; then
-    touch "$HOME/.bash_profile"
-fi
-
-# Verifica si el alias ya está en ~/.bash_profile
-if ! grep -qF "$SAIL_ALIAS" "$HOME/.bash_profile"; then
-    # Agrega el alias al archivo ~/.bash_profile
-    echo "$SAIL_ALIAS" >> "$HOME/.bash_profile"
-fi
-
-# Muestra un mensaje de confirmación
-echo "El alias 'sail' se ha agregado a ~/.bashrc y ~/.bash_profile."
 
 # Comprobar si PHP está instalado
 if ! command -v php &>/dev/null; then
     echo "PHP no está instalado. Por favor, instala PHP y vuelve a ejecutar el script."
-    sudo add-apt-repository ppa:ondrej/php
-    sudo apt update
-    sudo apt install php8.2
-    sudo phpenmod curl
-    sudo service php8.2-fpm restart
     exit 1
 fi
-sudo apt install php8.2-curl
-# Habilitar las extensiones
+
+# Comprobar si Docker está en ejecución
+if ! docker --version &>/dev/null; then
+    echo "Docker no está instalado o no se está ejecutando. Por favor, instala Docker y asegúrate de que esté en ejecución."
+    exit 1
+fi
+
+# Composer install
+composer install
+
+# Definir el alias 'sail' si no está definido
+SAIL_ALIAS='alias sail="[ -f sail ] && sh sail || sh vendor/bin/sail"'
+
+# Verificar si el archivo ~/.bashrc existe y si no, crearlo
+if [ ! -f "$HOME/.bashrc" ]; then
+    touch "$HOME/.bashrc"
+fi
+
+# Verificar si el alias 'sail' ya está en ~/.bashrc
+if ! grep -qF "$SAIL_ALIAS" "$HOME/.bashrc"; then
+    echo "$SAIL_ALIAS" >> "$HOME/.bashrc"
+fi
+
+# Verificar si el archivo ~/.bash_profile existe y si no, crearlo
+if [ ! -f "$HOME/.bash_profile" ]; then
+    touch "$HOME/.bash_profile"
+fi
+
+# Verificar si el alias 'sail' ya está en ~/.bash_profile
+if ! grep -qF "$SAIL_ALIAS" "$HOME/.bash_profile"; then
+    echo "$SAIL_ALIAS" >> "$HOME/.bash_profile"
+fi
+
+# Habilitar extensiones de PHP
 extensions_to_enable=(
     opcache
     pdo
@@ -78,17 +72,23 @@ extensions_to_enable=(
     sysvshm
     tokenizer
 )
+
 for extension in "${extensions_to_enable[@]}"; do
     sudo phpenmod -v 8.2 -s cli "$extension"
 done
+
+# Reiniciar el servicio PHP-FPM (ajusta esto según tu sistema)
+sudo service php8.2-fpm restart
+
+# Ejecutar comandos adicionales de Laravel Sail
 php artisan sail:add
 php artisan sail:install --devcontainer
-# Comprobar si Composer está instalado
 
-# Cargar cambios en la sesión actual
+# Recargar los cambios en la sesión actual
 source "$HOME/.bashrc"
 source "$HOME/.bash_profile"
 
+# Mensaje de confirmación
 echo -e "
     \n
     \n
@@ -113,9 +113,8 @@ E::::::::::::::::::::Em::::m   m::::m   m::::m a::::::::::aa:::a d::::::::::::::
 EEEEEEEEEEEEEEEEEEEEEEmmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaa  ddddddddd   ddddd    eeeeeeeeeeeeee           vvv
     ${NOCOLOR}Sígueme en https://github.com/emanuelcastillo
 
-    ejecuta -> sail composer install 
-    para levantar el sitio web de sail
-
+    Ejecuta 'sail composer install' para levantar el sitio web de Sail
 "
 
+# Iniciar Docker Compose
 docker compose up -d
